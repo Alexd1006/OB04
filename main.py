509e4104b9ff186903f2dd6381074d17a7650664
Alexd1006 <alex3nocode@gmail.com>
2025-03-12 16:10:36 +0300

    new

---
+++ main.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+class Weapon(ABC):
+    @abstractmethod
+    def attack(self,weapon):
+        pass
+
+class Sword(Weapon):
+    def attack(self):
+        return "Боец выбирает меч.\nБоец наносит удар мечом."
+
+class Bow(Weapon):
+    def attack(self):
+        return "Боец выбирает меч.\nБоец наносит удар мечом."
+
+
+class Fighter():
+    def __init__(self,name,weapon: Weapon):
+        self.name = name
+        self.weapon = weapon
+    def change_weapon(self, new_weapon: Weapon):
+        self.weapon = new_weapon
+    def attack(self):
+        return self.weapon.attack()
+
+
+class Monster:
+    def __init__(self, health=100):
+        self.health = health
+
+    def take_damage(self, damage=100):
+        self.health -= damage
+        if self.health <= 0:
+            return "Монстр побежден!"
+        else:
+            return f"Монстр получил урон, осталось {self.health} здоровья."
+
+fighter = Fighter("Герой", Sword())
+monster = Monster(150)
+
+
+
+print(fighter.attack())
+print(monster.take_damage())
+
+fighter.change_weapon(Bow())
+print(fighter.attack())
+print(monster.take_damage())
